cac2a764ec8d888ec8c5e674f7d39f2f6ccd4537
pinto119 <menukapinto5555@gmail.com>
2020-09-28 17:30:03 +0530

    Update version

---
+++ trying paralax/pscript.js
@@ -0,0 +1,4 @@
+//nav menu list opening and closing
+document.querySelector(".nav_menu_icon").addEventListener("click", () =>{
+  document.querySelector(".container").classList.toggle("change");
+});
